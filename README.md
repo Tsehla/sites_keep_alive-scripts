979f76b92c90ee1b7847ec56463d84c49ad39c45
tsehla <petrusnk@gmail.com>
2023-12-01 16:13:31 +0200

    mib

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # sites_keep_alive-scripts
+
+websites keep awake script 
+
+
+This script defines an array linksToPing containing the URLs you want to ping. It uses the axios library to make HTTP requests and the node-cron library to schedule the task to run every ten minutes. The pingAllLinks function iterates over the links and pings each one using the pingLink function.
+
+Save this script as, for example, ping-script.js and run it using:
---
+++ index.js
@@ -0,0 +1,35 @@
+const axios = require('axios');
+const cron = require('node-cron');
+
+// List of links to ping
+const linksToPing = [
+  'https://infolexicon.onrender.com/',
+  'https://wikiachive.onrender.com/',
+  // Add more links as needed
+];
+
+// Function to ping a single link
+async function pingLink(link) {
+  try {
+    const response = await axios.get(link);
+    console.log(`Pinged ${link} - Status: ${response.status}`);
+  } catch (error) {
+    console.error(`Error pinging ${link}: ${error.message}`);
+  }
+}
+
+// Function to ping all links
+function pingAllLinks() {
+  linksToPing.forEach((link) => {
+    console.log('Pinging links... : ', link);
+    pingLink(link);
+  });
+}
+
+// Schedule the task to run every ten minutes
+cron.schedule('*/10 * * * *', () => {
+  // console.log('Pinging links...');
+  pingAllLinks();
+});
+
+console.log('Pinging script started. Press Ctrl+C to exit.');
